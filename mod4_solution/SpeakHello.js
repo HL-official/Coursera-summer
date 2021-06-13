df3c9c5d1aaf98efa0832518a3658b785824086b
Harsh Ladani <84271484+HL-official@users.noreply.github.com>
2021-06-13 12:29:13 +0530

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -8,17 +8,13 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-(function (window){
-  var speakWord = "Hello";
-
-  var helloSpeaker={
-
-    speak: function (name) {
-    console.log(speakWord + " " + name);
-    }
-
-  };
-  window.helloSpeaker=helloSpeaker;
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
 })(window);
 
 
